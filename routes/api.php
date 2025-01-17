3fda0b212e637b65a2e85933047cfcdfd8691336
Dương Tùng Anh <tunnaduong@gmail.com>
2025-01-17 14:59:09 +0700

    Organize API routes under versioning prefix v2.0

--- routes/api.php
+++ routes/api.php
@@ -22,34 +22,37 @@ use App\Http\Controllers\NotificationController;
 | be assigned to the "api" middleware group. Make something great!
 |
 */
-Route::post('/login', [AuthController::class, 'login']);
 
-Route::get('/users/avatar/{username}', [UserController::class, 'getAvatar']);
-Route::get('/users/birthday', [BirthdayController::class, 'index']);
-Route::get('/users/list', [UserController::class, 'index']);
-Route::get('/users/online', [UserController::class, 'updateLastActivity']);
+Route::prefix('v2.0')->group(function () {
+    Route::post('/login', [AuthController::class, 'login']);
 
-Route::get('/radio/idle', [LiveRadioController::class, 'getIdlePlaylist']);
-Route::any('/radio/chatlogs', [LiveRadioController::class, 'handleLogs']);
+    Route::get('/users/avatar/{username}', [UserController::class, 'getAvatar']);
+    Route::get('/users/birthday', [BirthdayController::class, 'index']);
+    Route::get('/users/list', [UserController::class, 'index']);
+    Route::get('/users/online', [UserController::class, 'updateLastActivity']);
 
-Route::any('/notification/image', [NotificationController::class, 'getImages']);
-Route::get('/notification/list', [NotificationController::class, 'getNotifications']);
-Route::any('/notification/token', [NotificationController::class, 'updateToken']);
-Route::get('/notification/send', [NotificationController::class, 'sendNotification']);
+    Route::get('/radio/idle', [LiveRadioController::class, 'getIdlePlaylist']);
+    Route::any('/radio/chatlogs', [LiveRadioController::class, 'handleLogs']);
 
-Route::get('/gallery', [GalleryController::class, 'index']);
-Route::get('/gallery/photos', [GalleryController::class, 'getPhotos']);
-Route::get('/gallery/videos', [GalleryController::class, 'getVideos']);
+    Route::any('/notification/image', [NotificationController::class, 'getImages']);
+    Route::get('/notification/list', [NotificationController::class, 'getNotifications']);
+    Route::any('/notification/token', [NotificationController::class, 'updateToken']);
+    Route::get('/notification/send', [NotificationController::class, 'sendNotification']);
 
-Route::get('/feed/list', [FeedController::class, 'index']);
-Route::any('/feed/likes/add', [FeedController::class, 'handleLikes']);
-Route::get('/feed/likes', [FeedController::class, 'getLikes']);
+    Route::get('/gallery', [GalleryController::class, 'index']);
+    Route::get('/gallery/photos', [GalleryController::class, 'getPhotos']);
+    Route::get('/gallery/videos', [GalleryController::class, 'getVideos']);
 
-Route::any('/chat/conversations', [ChatController::class, 'sendMessage']);
-Route::any('/chat/messages', [ChatController::class, 'getMessages']);
-Route::get('/chat/home', [ChatController::class, 'getConversations']);
-Route::post('/chat/image', [ChatController::class, 'uploadImage']);
-Route::get('/chat/last-chat', [ChatController::class, 'getLastMessage']);
-Route::get('/chat/online', [ChatController::class, 'getActivity']);
+    Route::get('/feed/list', [FeedController::class, 'index']);
+    Route::any('/feed/likes/add', [FeedController::class, 'handleLikes']);
+    Route::get('/feed/likes', [FeedController::class, 'getLikes']);
 
-Route::get('/calendar/list', [CalendarController::class, 'getCalendarEvents']);
+    Route::any('/chat/conversations', [ChatController::class, 'sendMessage']);
+    Route::any('/chat/messages', [ChatController::class, 'getMessages']);
+    Route::get('/chat/home', [ChatController::class, 'getConversations']);
+    Route::post('/chat/image', [ChatController::class, 'uploadImage']);
+    Route::get('/chat/last-chat', [ChatController::class, 'getLastMessage']);
+    Route::get('/chat/online', [ChatController::class, 'getActivity']);
+
+    Route::get('/calendar/list', [CalendarController::class, 'getCalendarEvents']);
+});
